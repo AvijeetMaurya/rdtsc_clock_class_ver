bfcc6af6bb88e194a15792e7cf1c1e6cf1dd2b63
Avijeet Maurya <avijeet@zanskar.xyz>
2024-01-12 11:03:37 +0530

    Initial commit

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 
 #include "rdtsc_clock.hpp"
 
+#include <time.h>
+
 int main() {
-    RDTSC_Clock::init();
-    while (true) {  
-        auto now = std::chrono::system_clock::now();
-        long long rdtscp_timestamp = RDTSC_Clock::now();
-        long long system_timestamp = now.time_since_epoch().count();
-        std::cout << "Delta: " << rdtscp_timestamp - system_timestamp << '\n';
+    //RDTSC_Clock::init();
+    timespec ts;
+    int count = 10000000;
+    while (count--) {  
+        //auto now = std::chrono::system_clock::now();
+        //long long rdtscp_timestamp = RDTSC_Clock::now();
+        clock_gettime(CLOCK_REALTIME, &ts);
+        volatile long long system_timestamp = static_cast<long long>(ts.tv_sec*1e9) + ts.tv_nsec;
+        //std::cout << "Delta: " << rdtscp_timestamp - system_timestamp << '\n';
     }
-    RDTSC_Clock::exit();
+    //RDTSC_Clock::exit();
 }
